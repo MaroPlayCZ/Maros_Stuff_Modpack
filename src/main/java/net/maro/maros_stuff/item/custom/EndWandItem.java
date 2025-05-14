b2599d57aaebd0c01bab5daa22bf67348a1e919f
MaroPlayCZ <marca.musil@gmail.com>
2025-05-14 21:58:51 +0200

    1

---
+++ src/main/java/net/maro/maros_stuff/item/custom/EndWandItem.java
@@ -0,0 +1,12 @@
+package net.maro.maros_stuff.item.custom;
+
+import net.minecraft.client.gui.screens.social.PlayerEntry;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+
+public class EndWandItem extends Item {
+    public EndWandItem(Properties pProperties) {
+        super(pProperties);
+    }
+
+}
