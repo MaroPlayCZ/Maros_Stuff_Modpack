d0eedad8814f96721b6337adad5fe0faffb3b880
MaroPlayCZ <marca.musil@gmail.com>
2025-05-22 18:02:27 +0200

    wand complet

--- src/main/java/net/maro/maros_stuff/item/custom/EndWandItem.java
+++ src/main/java/net/maro/maros_stuff/item/custom/EndWandItem.java
@@ -1,6 +1,8 @@
 package net.maro.maros_stuff.item.custom;
 
+import net.maro.maros_stuff.item.ModItems;
 import net.minecraft.client.gui.screens.social.PlayerEntry;
+import net.minecraft.client.resources.model.Material;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
@@ -16,6 +18,8 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
+import net.minecraftforge.client.model.obj.ObjMaterialLibrary;
+import net.minecraftforge.fml.common.Mod;
 
 public class EndWandItem extends Item {
     public EndWandItem(Properties pProperties) {
@@ -24,6 +28,7 @@ public class EndWandItem extends Item {
 
     public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pHand) {
         ItemStack itemstack = pPlayer.getItemInHand(pHand);
+
         pLevel.playSound((Player)null, pPlayer.getX(), pPlayer.getY(), pPlayer.getZ(), SoundEvents.ENDER_PEARL_THROW, SoundSource.NEUTRAL, 0.5F, 0.4F / (pLevel.getRandom().nextFloat() * 0.4F + 0.8F));
         pPlayer.getCooldowns().addCooldown(this, 30);
         if (!pLevel.isClientSide) {
@@ -31,7 +36,7 @@ public class EndWandItem extends Item {
             thrownenderpearl.setItem(new ItemStack(Items.ENDER_PEARL));
             thrownenderpearl.shootFromRotation(pPlayer, pPlayer.getXRot(), pPlayer.getYRot(), 0.0F, 2F, 2F);
             pLevel.addFreshEntity(thrownenderpearl);
-            damageItem(itemstack,1,pPlayer,player -> player.setItemInHand(pHand, itemstack));
+            pPlayer.getItemInHand(pHand).hurtAndBreak(1,pPlayer,player -> player.broadcastBreakEvent(pHand));
         }
 
         return InteractionResultHolder.sidedSuccess(itemstack, pLevel.isClientSide());
@@ -39,7 +44,7 @@ public class EndWandItem extends Item {
 
     @Override
     public void onCraftedBy(ItemStack pStack, Level pLevel, Player pPlayer) {
-        pLevel.playSound((Player) null, pPlayer.getX(), pPlayer.getY(), pPlayer.getZ(), SoundEvents.END_GATEWAY_SPAWN, SoundSource.NEUTRAL, 0.5F, 0.4F / (pLevel.getRandom().nextFloat() * 0.4F + 0.8F));
+        pLevel.playSound((Player) null, pPlayer.getX(), pPlayer.getY(), pPlayer.getZ(), SoundEvents.END_PORTAL_SPAWN, SoundSource.NEUTRAL, 0.5F, 0.4F / (pLevel.getRandom().nextFloat() * 0.4F + 0.8F));
         super.onCraftedBy(pStack, pLevel, pPlayer);
     }
 }
