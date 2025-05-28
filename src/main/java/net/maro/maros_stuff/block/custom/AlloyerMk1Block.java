9657b6ef55843b40b3937af931acc965e37ee5de
MaroPlayCZ <marca.musil@gmail.com>
2025-05-28 16:08:44 +0200

    alloyer wip2

--- src/main/java/net/maro/maros_stuff/block/custom/AlloyerMk1Block.java
+++ src/main/java/net/maro/maros_stuff/block/custom/AlloyerMk1Block.java
@@ -1,19 +1,40 @@
 package net.maro.maros_stuff.block.custom;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.MenuProvider;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.AbstractFurnaceBlock;
 import net.minecraft.world.level.block.BaseEntityBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.Nullable;
 
-public class AlloyerMk1Block extends BaseEntityBlock {
+public class AlloyerMk1Block extends AbstractFurnaceBlock implements MenuProvider {
     protected AlloyerMk1Block(Properties pProperties) {
         super(pProperties);
     }
 
     @Override
+    protected void openContainer(Level pLevel, BlockPos pPos, Player pPlayer) {
+
+    }
+
+    @Override
     public @Nullable BlockEntity newBlockEntity(BlockPos pPos, BlockState pState) {
+        return null;
+    }
+
+    @Override
+    public Component getDisplayName() {
+        return null;
+    }
+
+    @Override
+    public @Nullable AbstractContainerMenu createMenu(int pContainerId, Inventory pPlayerInventory, Player pPlayer) {
         return null;
     }
 }
--- src/main/java/net/maro/maros_stuff/block/entity/AlloyerMk1BlockEntity.java
+++ src/main/java/net/maro/maros_stuff/block/entity/AlloyerMk1BlockEntity.java
@@ -7,14 +7,16 @@ import net.minecraft.world.MenuProvider;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.item.crafting.RecipeType;
+import net.minecraft.world.level.block.entity.AbstractFurnaceBlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.Nullable;
 
-public class AlloyerMk1BlockEntity extends BlockEntity implements MenuProvider {
+public class AlloyerMk1BlockEntity extends AbstractFurnaceBlockEntity implements MenuProvider {
     public AlloyerMk1BlockEntity(BlockEntityType<?> pType, BlockPos pPos, BlockState pBlockState) {
-        super(pType, pPos, pBlockState);
+        super(pType,pPos,pBlockState, RecipeType.BLASTING);
     }
 
     @Override
@@ -23,7 +25,17 @@ public class AlloyerMk1BlockEntity extends BlockEntity implements MenuProvider {
     }
 
     @Override
+    protected Component getDefaultName() {
+        return null;
+    }
+
+    @Override
     public @Nullable AbstractContainerMenu createMenu(int pContainerId, Inventory pPlayerInventory, Player pPlayer) {
+        return null;
+    }
+
+    @Override
+    protected AbstractContainerMenu createMenu(int pContainerId, Inventory pInventory) {
         return null;
     }
 }
