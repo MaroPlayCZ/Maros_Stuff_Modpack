3bd31a0460a5d3e7067a2e9179e09741b5df240f
MaroPlayCZ <marca.musil@gmail.com>
2025-06-20 14:58:03 +0200

    end_wand_empty - stack size fix

--- src/main/java/net/maro/maros_stuff/item/ModItems.java
+++ src/main/java/net/maro/maros_stuff/item/ModItems.java
@@ -27,7 +27,7 @@ public class ModItems {
     public static final RegistryObject<Item> ENDER_ALLOY_INGOT = ITEMS.register("ender_alloy_ingot",
             () -> new Item(new Item.Properties()));
     public static final RegistryObject<Item> END_WAND_EMPTY = ITEMS.register("end_wand_empty",
-            () -> new Item(new Item.Properties()));
+            () -> new Item(new Item.Properties().stacksTo(1)));
     public static final RegistryObject<Item> END_WAND = ITEMS.register("end_wand",
             () -> new EndWandItem(new Item.Properties().defaultDurability(4)));
 
